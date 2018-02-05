c99c050f235307bfdb77748d4af054de0601bda2
Lillian Chernin <l.smith1654@gmail.com>
2018-02-05 13:54:08 -0600

    enable api access

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ try {
   ENV = process.env;
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET");
+  next();
+});
+
 mongoose.connect(ENV.MONGODB_URI);
 
 const app = express();
